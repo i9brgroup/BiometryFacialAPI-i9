113a9d063007643fbdd7bb70d90701f7ebd0fd8d
i9junior <joselito.barreto@i9brgroup.com.br>
2026-03-16 10:39:01 -0300

    refactor: Move arquivos para novas pastas.

--- src/main/java/com/i9brgroup/jbarreto/facial_auth_i9/infrastructure/config/JacksonConfig.java
+++ src/main/java/com/i9brgroup/jbarreto/facial_auth_i9/infrastructure/config/JacksonConfig.java
@@ -1,4 +1,4 @@
-package com.i9brgroup.jbarreto.facial_auth_i9.infra.config;
+package com.i9brgroup.jbarreto.facial_auth_i9.infrastructure.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
