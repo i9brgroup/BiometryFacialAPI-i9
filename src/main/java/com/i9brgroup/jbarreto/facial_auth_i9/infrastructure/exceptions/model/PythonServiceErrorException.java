a6bea2c342d1f54e7e00f01514e233078cfe6b25
i9junior <joselito.barreto@i9brgroup.com.br>
2026-03-26 16:58:44 -0300

    refactor: Refatora todos os lançamentos de exceptions para exceptions especificas para cada dominio e erros diferentes.

---
+++ src/main/java/com/i9brgroup/jbarreto/facial_auth_i9/infrastructure/exceptions/model/PythonServiceErrorException.java
@@ -0,0 +1,8 @@
+package com.i9brgroup.jbarreto.facial_auth_i9.infrastructure.exceptions.model;
+
+public class PythonServiceErrorException extends RuntimeException {
+
+    public PythonServiceErrorException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/i9brgroup/jbarreto/facial_auth_i9/infrastructure/exceptions/model/RollbackException.java
@@ -0,0 +1,8 @@
+package com.i9brgroup.jbarreto.facial_auth_i9.infrastructure.exceptions.model;
+
+public class RollbackException extends RuntimeException {
+
+    public RollbackException(String message) {
+        super(message);
+    }
+}
